65cf123c3d2af13e1afc9b097fb00f24d3aa2936
Omkar Bende <136046556+omkar7b@users.noreply.github.com>
2023-07-15 16:32:04 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -38,13 +38,24 @@ header.style.border = 'solid 3px #000' */
 
 
 
-console.log(document.getElementsByClassName('title'))
+/*console.log(document.getElementsByClassName('title'))
 
 var title = document.getElementsByClassName('title')
 
+title.innertext = 'title'
 //console.log(main[1]);
 console.log(title)
 console.log(title[0]);
 
 title[0].style.color = 'green'
 title[0].style.fontWeight = 'bold'
+title.innertext = 'title'*/
+
+var items = document.getElementsByClassName('list-group-item')
+console.log(items[2]);
+
+items[2].style.background = 'green'
+
+for(let i=0;i< items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
